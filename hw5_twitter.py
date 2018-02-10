bab33eb4640512a07c16febad01ae132be6bed68
pansyjia <siyujia@umich.edu>
2018-02-10 16:15:23 -0500

    part 3 revised

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -3,7 +3,8 @@ import json
 import sys
 import requests
 import secret_data # file that contains OAuth credentials
-import nltk # uncomment line after you install nltk
+import nltk
+import ssl
 
 ## SI 507 - HW05
 ## Name: Siyu Jia (uniqname: siyujia)
@@ -28,11 +29,11 @@ requests.get(url, auth = auth)
 #Write your code below:
 #Code for Part 3:Caching
 #Finish parts 1 and 2 and then come back to this
-CACHE_FNAME = "twitter_cache.json"
+CACHE_TWITTER = "twitter_cache.json"
 
 ## check if data is fetched from the cache
 try:
-    cache_file = open(CACHE_FNAME, 'r')
+    cache_file = open(CACHE_TWITTER, 'r')
     cache_json = cache_file.read()
     CACHE_DICTION = json.loads(cache_json)
     cache_file.close()
@@ -42,11 +43,11 @@ except:
 ## generate a unique id
 def params_unique_combination(base_url, params_dict):
     alphabetized_keys = sorted(params_dict.keys())
-
     res = []
     for key in alphabetized_keys:
         res.append("{}-{}".format(key, params_dict[key]))
-    return base_url + "_".join(res)
+        unique_ident = base_url + "_".join(res)
+    return unique_ident
 
 
 ## get data from cached file or Twitter
@@ -57,6 +58,7 @@ def get_from_twitter(username, num_tweets):
     params_dict["screen_name"] = username
     params_dict["count"] = num_tweets
 
+    # generate a unique id
     unique_ident = params_unique_combination(base_url, params_dict)
 
     if unique_ident in CACHE_DICTION:
@@ -66,16 +68,10 @@ def get_from_twitter(username, num_tweets):
         print("Fetching data from Twitter...")
         response = requests.get(url = base_url, params = params_dict, auth = auth)
         resp_lst = json.loads(response.text) #resp_lst is a list of dictionaries(tweets)
-        
-
-        resp_dict = {}
-        resp_dict["statuses"] = resp_lst
-        CACHE_DICTION[unique_ident]= resp_dict
-
-        write_file = open(CACHE_FNAME, 'w')
+        write_file = open(CACHE_TWITTER, 'w')
         write_file.write(json.dumps(CACHE_DICTION))
         write_file.close()
-        return CACHE_DICTION[unique_ident]
+        return resp_lst
 
 
 
@@ -89,34 +85,42 @@ results_dict = get_from_twitter(username, num_tweets)
 ## write the json to a file
 # resp_file = "tweets.json"
 # f = open(resp_file, "w")
-# resp_str = json.dumps(results_dict, indent = 2)
+# resp_str = json.dumps(results_dict)
 # f.write (resp_str)
 # f.close()
 
 
 
 #Code for Part 2:Analyze Tweets
-# for tweet in results_dict["statuses"]:
+# for tweet in results_dict:
 #     tokens = nltk.word_tokenize(tweet["text"])
 #     words_dict = nltk.FreqDist(tokens)
 
 words_lst = []
 stopwords_lst = ["http", "https", "RT"]
 
-for tweet in results_dict["statuses"]:
+for tweet in results_dict:
     tokens = nltk.word_tokenize(tweet["text"])
+
+    # iterate through the tokenized words and filter out the stopwords
     for word in tokens:
         if word[0].isalpha() and word not in stopwords_lst:
             words_lst.append(word)
+            # print (words_lst)
 
 words_dict = nltk.FreqDist(words_lst)
 
 sorted_words_lst = sorted(words_dict.items(), key = lambda x: x[1], reverse = True)
 
+print("USER: ", username)
+print("TWEETS ANALYZED: ", num_tweets)
 print("THE 5 MOST FREQUENTLY USED WORDS: ")
 for word_tuple in sorted_words_lst[0:5]:
     word, frequency = word_tuple
-    print(word, ": ", frequency, "times")
+    print(word, "(",frequency,")")
+
+
+
 
 
 if __name__ == "__main__":
